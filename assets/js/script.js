7a4bc8f52269e502cecdbfd436d5557f7847af06
TammyGirl2015 <>
2024-01-22 19:35:57 +0000

    add more css styling and images

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,4 +32,21 @@ function NextQuestion(questionNumber) {
 function handleNextQuestion() {
     currentQuestion++;
     NextQuestion(currentQuestion);
-} 
+}
+
+document.getElementById('generalKnowledgeButton').addEventListener('click', handleGeneralKnowledge);
+
+var img = document.createElement("img");
+img.src = "assets/images/milad-fakurian-58Z17lnVS4U-unsplash.jpg"; alt = "picture of a brain";
+var div = document.getElementById("gk-img");
+div.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "assets/images/pexels-anton-h-145707.jpg"; alt = "record player";
+var div = document.getElementById("music-img");
+div.appendChild(img);
+
+var img = document.createElement("img");
+img.src = "assets/images/pexels-donald-tong-66134.jpg"; alt = "video camera recording";
+var div = document.getElementById("movies-img");
+div.appendChild(img);
